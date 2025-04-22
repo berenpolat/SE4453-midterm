adfecff1f0da9430e8d2f1018c5e5ee3ad3af7ee
sssseaurchin <iloveleftfordeadtwo@gmail.com>
2025-04-22 19:54:20 +0300

    Initial commit with existing Java Spring Boot project.

---
+++ src/main/java/com/midterm/se4453_midterm/Se4453MidtermApplication.java
@@ -0,0 +1,13 @@
+package com.midterm.se4453_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	Se4453MidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se4453MidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/midterm/se4453_midterm/Se4453MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.se4453_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se4453MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
